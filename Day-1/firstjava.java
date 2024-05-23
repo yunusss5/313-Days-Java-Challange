7598ffc6f7dca83cd4826ac090ee6bb922b19052
yunusss5 <yunusmr435@gmail.com>
2024-05-23 22:42:40 +0530

    First Day

---
+++ Day-1/firstjava.java
@@ -0,0 +1,10 @@
+// Basic Of Java
+//Acces My HandWritten Notes from my GoogleDrive link 
+
+// 1. syntax
+
+public class firstjava{
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+}
+}
