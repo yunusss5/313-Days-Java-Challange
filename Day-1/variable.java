88c29543717401fc682566dc8e96ae0be597d6b1
yunusss5 <yunusmr435@gmail.com>
2024-05-23 23:06:26 +0530

    Variable in Java these six variable int,float,char,double,String,boolean

---
+++ Day-1/variable.java
@@ -0,0 +1,17 @@
+public class variable {
+    public static void main(String[] args){
+        int amount=50;
+        float percentage= 68.57f;
+        double myvar=50.22;
+        char code='A';
+        String name="@yunusss5";
+        boolean answer=true;
+
+        System.out.println("This is amount - integer variable : " + amount);
+        System.out.println("This is percentage - float variable : " + percentage);
+        System.out.println("This is myvar - double variable : " + myvar);
+        System.out.println("This is code - char variable : " + code);
+        System.out.println("This is name - String variable : " + name);
+        System.out.println("This is answer - boolean variable : " + answer);
+    }
+}
