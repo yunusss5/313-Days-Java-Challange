579452017a14e02694b695f1a7d06917fe4dd870
yunusss5 <yunusmr435@gmail.com>
2024-05-24 22:48:32 +0530

    Day-2 operator in java

---
+++ Day-2/operator.java
@@ -0,0 +1,19 @@
+public class operator {
+    public static void main(String[] args) {
+        System.out.println("1. Arithmetic Operator-");
+        int a=5;
+        int b=10;
+        int add=a+b;
+        int substract=a-b;
+        int multiplication=a*b;
+        int division=b/a;
+        int module=b%a;
+        System.out.println("Addition(+) of a b: "+ add);
+        System.out.println("Substraction(-) of a b: "+ substract);
+        System.out.println("Multiplication(*) of a b: "+ multiplication);
+        System.out.println("Division(/) of a b: "+ division);
+        System.out.println("Module(%) of a b: "+ module);
+
+    }
+    public void
+}
