f23cfcdb123bdce6d55260a23edfe30e658c1477
yunusss5 <yunusmr435@gmail.com>
2024-06-09 19:59:31 +0530

    Return

---
+++ Day-8/Return.java
@@ -0,0 +1,10 @@
+
+public class Return {
+    public static void main(String[] args) {
+       int add= sum(10, 5);
+       System.out.println("Sum- Substracted with 5 is " + (add-5));
+    }
+    static int sum(int a, int b){
+        return a+b;
+    }
+}
