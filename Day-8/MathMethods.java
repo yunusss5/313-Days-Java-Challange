4eb4a803958414e37c3bf264c282b1542dd2c29b
yunusss5 <yunusmr435@gmail.com>
2024-06-09 20:42:20 +0530

    Math Methods

---
+++ Day-8/MathMethods.java
@@ -0,0 +1,23 @@
+
+public class MathMethods {
+    public static void main(String[] args) {
+        int a=4;
+        int b=6;
+        int c=2;
+
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.pow(a, c));
+        System.out.println(Math.random());
+        System.out.println(Math.ceil(5.9));
+        System.out.println(Math.ceil(-5.9));
+        System.out.println(Math.floor(4.9));
+        System.out.println(Math.round(5.1));//5
+        System.out.println(Math.round(5.6));//6
+
+
+        // .... and more
+
+    }
+}
