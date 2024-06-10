d4ba9fb766b2facd843e8abddf35cefc9a540e08
yunusss5 <yunusmr435@gmail.com>
2024-06-10 15:29:29 +0530

    Input String

--- Day-9/String1.java
+++ Day-9/String1.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 
 public class String1 {
-    
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    System.out.println("Enter Your First Name: ");
+    String firstName=sc.nextLine();
+
+    System.out.println("Enter Your Last Name: ");
+    String lastName=sc.nextLine();
+
+    System.out.println("Your Full Name is "+firstName+" "+lastName);
+  }  
 }
